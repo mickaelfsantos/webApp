fbf5cc2b8c07eccb2d3c6be56597f319128b0c88
mickaelsantos <mickaelfsantos@gmail.com>
2020-07-24 14:21:14 +0100

    mongoURI alterada

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://admin:123123123@cluster0.ua38y.mongodb.net/<dbname>?retryWrites=true&w=majority/webApp"}
+    module.exports = {mongoURI: "mongodb+srv://admin:123123123@cluster0.ua38y.mongodb.net/webApp?retryWrites=true&w=majority"}
 }
 else{
     module.exports = {mongoURI: "mongodb://localhost/webApp"}
