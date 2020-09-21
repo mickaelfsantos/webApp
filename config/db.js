070be4a4a454834800382e359f8de08afd98a197
mickaelsantos <mickaelfsantos@gmail.com>
2020-09-21 13:55:18 +0100

    teste

--- config/db.js
+++
@@ -1,6 +0,0 @@
-if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://admin:123123123@cluster0.ua38y.mongodb.net/webApp?retryWrites=true&w=majority"}
-}
-else{
-    module.exports = {mongoURI: "mongodb://localhost/webApp"}
-}
